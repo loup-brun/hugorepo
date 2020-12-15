531a8cb72c33304871d79ec99a05dd98287fc3a6
Louis-Olivier Brassard <louis@loupbrun.ca>
2020-12-14 20:04:32 -0500

    fix postcss; cleanup Makefile
    Also re-introduce purgecss

--- Makefile
+++ Makefile
@@ -2,10 +2,8 @@
 
 help:
 	@echo "Usage: make <command>"
-	@echo "  n       Create a new note."
 	@echo "  serve   Runs a development webserver on port 1313"
 	@echo "  build   Build the site with minification."
-	@echo "  build-staging   Build the site with *staging* configuration."
 	@echo "  deploy  Run the deployment."
 	@echo "  all     Build and deploy."
 
@@ -13,12 +11,12 @@ serve:
 	hugo serve --disableFastRender
 
 build:
-	npm run build:styles
+#	npm run build:styles
 	rm -rf public
 	hugo --minify
 
 deploy:
-	cmd/deploy.sh
+	commands/deploy.sh
 
 all:
 	make build
--- assets/styles/postcss.config.js
+++ assets/styles/postcss.config.js
@@ -1,3 +1,25 @@
+// PurgeCSS
+// example here: https://github.com/dirkolbrich/hugo-theme-tailwindcss-starter/blob/main/assets/css/postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    'layouts/**/*.html',
+    'content/**/*.{md,html}',
+    'public/**/*.html'
+  ],
+
+  // This is the function used to extract class names from your templates
+  defaultExtractor: content => {
+    // Capture as liberally as possible, including things like `h-(screen-1.5)`
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+    return broadMatches.concat(innerMatches)
+  }
+})
 
 module.exports = {    
   plugins: [
@@ -9,5 +31,7 @@ module.exports = {
       grid: true,
       overrideBrowserslist: ['>1%']
     }),    
-  ]
+  ],
+  // purge only in `production` environment
+  ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
 }
